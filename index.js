ba0744bdc29aedfb7e06bf323a819e9423309b0f
raul518 <raulmacoca402@gmail.com>
2022-06-20 12:40:10 +0200

    nuevos cambios en selenium

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const {Builder} = require("Selenium-webdriver");
+const {Builder, By, Key, until} = require ("selenium-webdriver");
 
-async function example() {
-  let driver = await new Builder().forBrowser("firefox").build();
-  
-    await driver.get("https://espanol.yahoo.com/?p=us");
-    
+async function ejemplo(){
+    let driver = await new Builder().forBrowser("firefox").build();
+    await driver.get("https://yahoo.com");
+
+    await driver.get("https://login.yahoo.com/?.lang=es-US&src=homepage&.done=https%3A%2F%2Fespanol.yahoo.com%2F%3Fp%3Dus&pspid=2142990676&activity=ybar-signin");
+     
+
 }
-example()
+ejemplo();
